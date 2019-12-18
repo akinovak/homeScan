fccb45c18ac1cdd03de1dfd99e370fe955a2edb4
popdav <popdav@outlook.com>
2019-12-18 13:35:55 +0100

    added history array

--- repository/mongo_repository.py
+++ repository/mongo_repository.py
@@ -1,5 +1,5 @@
 from datetime import date
-
+from random import randrange
 
 class MongoRepository:
     def __init__(self, mongo_adapter):
@@ -8,21 +8,25 @@ class MongoRepository:
     def _prepare_insert(self, o):
         to_insert = {}
         to_insert['link'] = o['link']
-        to_insert['istorija'] = {str(date.today()): o['cena']}
+        to_insert['istorija'] = [{
+            'date': str(date.today()),
+            'price': o['cena']
+        }]
+
         return to_insert
 
     def _prepare_string(self):
-        return 'istorija.' + str(date.today())
+        return str(date.today()) + str(randrange(20, 40))
 
     def handle_object(self, o, collection):
         found = self.mongo_adapter.find_one({'link': o['link']}, collection)
         if found:
             self.mongo_adapter.update_one(
                 {'link': o['link']},
-                {'cena': o['cena']}, collection)
+                {'$set': {'cena': o['cena']}}, collection)
             self.mongo_adapter.update_one(
                 {'link': o['link']},
-                {'$set': {self._prepare_string(): o['cena']}}, 'test_cene')
+                {'$push': {'istorija': {'date': self._prepare_string(), 'price': o['cena']}}}, 'test_cene')
         else:
             self.mongo_adapter.insert_one(o, collection)
             self.mongo_adapter.insert_one(self._prepare_insert(o), 'test_cene')
